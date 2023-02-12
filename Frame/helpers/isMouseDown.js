278c846069e325649768727f68dd366c8d990d0a
Evgene <mmagic88720@gmail.com>
2023-02-12 18:42:31 +0200

    Delete Frame directory

--- Frame/helpers/isMouseDown.js
+++
@@ -1,9 +0,0 @@
-
-
-export default function isMouseDown(mousedown) {
-    if (mousedown) {
-        document.documentElement.classList.add("unselectable")
-    } else {
-        document.documentElement.classList.remove("unselectable")
-    }
-}
--- Frame/helpers/secondsToPixels.js
+++
@@ -1,6 +0,0 @@
-
-export default function secondsToPixels (positionInSeconds, timeScaleState) {
-    const { pixelsPerDivision } = timeScaleState
-    // return Math.round(positionInSeconds * pixelsPerDivision)
-    return positionInSeconds * pixelsPerDivision
-}
